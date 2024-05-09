6660951c18722f195240f8236582eb42661d9bed
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-05-09 15:38:05 +0200

    Modify AccountClosedEvent

--- account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
+++ account.common/src/main/java/com/techbank/account/common/events/AccountClosedEvent.java
@@ -3,11 +3,17 @@ package com.techbank.account.common.events;
 import com.techbank.cqrs.core.events.BaseEvent;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class AccountClosedEvent extends BaseEvent {
+
+    public AccountClosedEvent(int version) {
+        super(version);
+    }
 
 }
