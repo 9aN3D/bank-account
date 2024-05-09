6e7b4c5842315360f33057641e18a9c0c4cc9392
Volodymyr Kovalchuk <volodymyr.kovalchuk@nvt.pl>
2024-05-09 11:21:39 +0200

    Add logs to AccountEventStore

--- account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventStore.java
+++ account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventStore.java
@@ -9,6 +9,7 @@ import com.techbank.cqrs.core.exceptions.ConcurrencyException;
 import com.techbank.cqrs.core.infrastructure.EventStore;
 import com.techbank.cqrs.core.producers.EventProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -16,6 +17,7 @@ import java.util.List;
 
 import static java.util.Objects.isNull;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AccountEventStore implements EventStore {
@@ -25,11 +27,14 @@ public class AccountEventStore implements EventStore {
 
     @Override
     public void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion) {
+        log.info("Saving events for {aggregateId: {}, eventsSize: {}, expectedVersion: {}}", aggregateId, events.spliterator().getExactSizeIfKnown(), expectedVersion);
+
         var eventStoreList = eventStoreRepository.findByAggregateIdentifier(aggregateId);
         if (expectedVersion != -1 && eventStoreList.get(eventStoreList.size() - 1).getVersion() != expectedVersion) {
             throw new ConcurrencyException();
         }
         var version = expectedVersion;
+        var persistedEventCount = 0;
         for (var event : events) {
             version++;
             event.setVersion(version);
@@ -37,19 +42,27 @@ public class AccountEventStore implements EventStore {
             var persistedEvent = eventStoreRepository.save(eventModel);
             if (!persistedEvent.getId().isEmpty()) {
                 eventProducer.produce(event.getClass().getSimpleName(), event);
+                persistedEventCount++;
             }
         }
+
+        log.info("Saved events for {aggregateId: {}, resultSize: {}, expectedVersion: {}", aggregateId, persistedEventCount, expectedVersion);
     }
 
     @Override
     public List<BaseEvent> getEvents(String aggregateId) {
+        log.trace("Getting events for {aggregateId: {}}", aggregateId);
+
         var eventStoreList = eventStoreRepository.findByAggregateIdentifier(aggregateId);
         if (isNull(eventStoreList) || eventStoreList.isEmpty()) {
             throw new AggregateNotFoundException("Incorrect account ID provided");
         }
-        return eventStoreList.stream()
+        var result = eventStoreList.stream()
                 .map(EventModel::getEventData)
                 .toList();
+
+        log.info("Got events for {aggregateId: {}, resultSize: {}}", aggregateId, result.size());
+        return result;
     }
 
     private EventModel buildEventModel(String aggregateId, BaseEvent event, int version) {
